8f992fa06b2be317d1c5081d5a4a69dbd925cc18
Panigrahi Bala Srivatsa <balasrivatsapanigrahi1@gmail.com>
2025-07-06 16:15:47 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PostWorkout from "./pages/PostWorkout";
+import CalendarPage from "./pages/CalendarPage";
+import "./styles/App.css";
+
+// Custom wrapper to use hooks like useLocation outside the main return
+function AppWrapper() {
+  const location = useLocation();
+
+  // Define routes that should NOT show the Navbar
+  const noNavbarRoutes = ["/", "/login", "/register"];
+
+  const showNavbar = !noNavbarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/post-workout" element={<PostWorkout />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  );
+}
+
+export default App;
